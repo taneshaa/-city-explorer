e8d677278450309123474ea4841a29218b505dd7
Tanesha Brester <taneshabrester@gmail.com>
2022-03-24 14:19:12 -0700

    finish lab06

--- src/Main.js
+++ src/Main.js
@@ -47,30 +47,37 @@ class Main extends React.Component {
     console.log(this.state.searchquery);
     return (
       <>
-    <Form onSubmit = {this.handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>City</Form.Label>
-        <Form.Control type="City" placeholder="Enter city" onChange={this.setCity} />
-        <Form.Text className="text-muted">
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>City</Form.Label>
+            <Form.Control type="City" placeholder="Enter city" onChange={this.setCity} />
+            <Form.Text className="text-muted">
 
-        </Form.Text>
+            </Form.Text>
 
 
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
 
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Explore!
-      </Button>
-    </Form >
-    <p>{this.state.cityName}</p>
-    <p>{this.state.latitude}</p>
-    <p>{this.state.longitude}</p>
-    <Map url={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_APIKEY}&center=${this.state.latitude},${this.state.longitude}&size=${window.innerWidth}x300&format=jpg&zoom=12`}/>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Explore!
+          </Button>
+        </Form >
+        {this.state.errorMessage ?
+          <p>{this.state.errorMessage}</p>
+          :
+          <div>
+            <p>{this.state.cityName}</p>
+            <p>{this.state.latitude}</p>
+            <p>{this.state.longitude}</p>
+            <Map url={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_APIKEY}&center=${this.state.latitude},${this.state.longitude}&size=${window.innerWidth}x300&format=jpg&zoom=12`} />
+          </div>
+  }
     </>
     )
   }
+
 }
 
-export default Main
+export default Main;
